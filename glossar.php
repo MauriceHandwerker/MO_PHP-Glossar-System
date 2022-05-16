640cea993f6fd002787897425fe10f3184a423f6
Maurice .H <66009198+WilderMO@users.noreply.github.com>
2022-05-16 16:13:01 +0200

    Update glossar.php

--- glossar.php
+++ glossar.php
@@ -1,7 +1,25 @@
 <?php
 
-include 'connection.php';
+echo "<table style='border: solid 1px black;'>";
+echo "<tr><th>Letter</th><th>Name</th><th>Description</th></tr>";
 
+class TableRows extends RecursiveIteratorIterator {
+  function __construct($it) {
+    parent::__construct($it, self::LEAVES_ONLY);
+  }
+
+  function current() {
+    return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+  }
+
+  function beginChildren() {
+    echo "<tr>";
+  }
+
+  function endChildren() {
+    echo "</tr>" . "\n";
+  }
+}
 ?>
 
 <!DOCKYPE html>
@@ -9,28 +27,81 @@ include 'connection.php';
     <h1>Glossar<h1>
 
     <section class="tables">
-        <table>
-            <tr>
-                <th>Letter</th>
-                <th>Name</th>
-                <th>Description</th>
-            <tr>
         <?php
-            $sql = "SELECT letter, name, description FROM glossary";
-            $result = $conn->query($sql);
+			$servername = "localhost";
+			$username = "root";
+			$password = "";
+			$dbname = "glossary";
 
-            if ($result->num_rows > 0) {
-            // output data of each row
-                while($row = $result->fetch_assoc()) {
-                    echo "<tr>";
-                    echo "  <td>". $row["letter"]. "</td>";
-                    echo "  <td>". $row["name"]. "</td>";
-                    echo "  <td>". $row["description"]. "</td>";
-                    echo "</tr>";
-                }
-            } else {
-                echo "0 results";
-            }
+			try {
+			  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+			  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			  $stmt = $conn->prepare("SELECT letter, name, description FROM input");
+			  $stmt->execute();
+
+			  // set the resulting array to associative
+			  $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+			  foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+				echo $v;
+			  }
+			} catch(PDOException $e) {
+			  echo "Error: " . $e->getMessage();
+			}
+			$conn = null;
+			echo "</table>";
+        ?>
+    </section>
+</html><?php
+
+echo "<table style='border: solid 1px black;'>";
+echo "<tr><th>Letter</th><th>Name</th><th>Description</th></tr>";
+
+class TableRows extends RecursiveIteratorIterator {
+  function __construct($it) {
+    parent::__construct($it, self::LEAVES_ONLY);
+  }
+
+  function current() {
+    return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+  }
+
+  function beginChildren() {
+    echo "<tr>";
+  }
+
+  function endChildren() {
+    echo "</tr>" . "\n";
+  }
+}
+?>
+
+<!DOCKYPE html>
+<html>
+    <h1>Glossar<h1>
+
+    <section class="tables">
+        <?php
+			$servername = "localhost";
+			$username = "root";
+			$password = "";
+			$dbname = "glossary";
+
+			try {
+			  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+			  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			  $stmt = $conn->prepare("SELECT letter, name, description FROM input");
+			  $stmt->execute();
+
+			  // set the resulting array to associative
+			  $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+			  foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+				echo $v;
+			  }
+			} catch(PDOException $e) {
+			  echo "Error: " . $e->getMessage();
+			}
+			$conn = null;
+			echo "</table>";
         ?>
     </section>
 </html>
