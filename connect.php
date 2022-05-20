7fecd6d5465ef873a6777e91a1b6d0540d0a7bea
Maurice .H <66009198+WilderMO@users.noreply.github.com>
2022-05-20 08:37:13 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,12 +1,11 @@
 <?php
 
 $servername = "localhost";
-$port = "1586";
-$username = "maurice";
-$password = "MOMO";
-$dbname = "glossary";
+$username = "username";
+$password = "password";
+$dbname = "MO_Glossar_System";
 try {
-	$conn = new PDO("pgsql:host=$servername;port=$port;dbname=$dbname", $username, $password);
+	$conn = new PDO("pgsql:host=$servername;dbname=$dbname", $username, $password);
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch(PDOException $e) {
 				echo "Error: " . $e->getMessage();
